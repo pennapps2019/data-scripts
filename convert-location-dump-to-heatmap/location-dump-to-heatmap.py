7374f3304d1f98445de8856b2d0e4feca21ee3aa
Jeffrey Leung <jleung513@gmail.com>
2019-09-08 01:45:45 -0400

    Change filenames and fix JSON formatting

--- convert-location-dump-to-heatmap/location-dump-to-heatmap.py
+++ convert-location-dump-to-heatmap/location-dump-to-heatmap.py
@@ -4,22 +4,22 @@
 # into data formatted for a Google Maps Heatmap.
 #
 # The input file must be in the same directory, named 'location-log.csv'.
-# The output file will be in the same directory, named 'location-heatmap.json'.
+# The output file will be in the same directory, named 'location_heatmap.json'.
 
 import csv
 
-output_filename = 'location-heatmap.json'
+output_filename = 'location_heatmap.json'
 
 def print_to_output(output_file, string):
-    print(string)
-    output_file.write(string + '\n')
+    print(string, end='')
+    output_file.write(string)
 
 output = open(output_filename, 'w')
-
 print_to_output(output, '[')
 
 with open('location-log.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
+    first_row = True;
     for row in csv_reader:
         # Skip empty lines
         if len(row) is 0:
@@ -30,10 +30,18 @@ with open('location-log.csv') as csv_file:
         lon = round(float(row[3]), 6)
 
         # Format as JSON
-        print_to_output(output, '    {"lat" : ' + str(lat) + ', "lng" : ' + str(lon) + ' },')
+        if first_row:
+            first_row = False
+        else:
+            print_to_output(output, ' ,')
 
-print_to_output(output, ']')
+        print_to_output(output, '\n{"lat" : ' + str(lat) + ', "lng" : ' + str(lon) + ' }')
+
+print_to_output(output, '\n]')
 output.close()
 
 print()
 print(output_filename)
+
+'''
+'''
